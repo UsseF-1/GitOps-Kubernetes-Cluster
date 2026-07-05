4def016fe24867616a15a12d2f7af2abed822306
shahdelwan0 <shahdelwan2024@gmail.com>
2026-07-06 02:43:34 +0300

    update EKS access configuration for GitHub Actions with specific IAM user ARN

--- terraform/main.tf
+++ terraform/main.tf
@@ -313,14 +313,14 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_eks_access_entry" "github_actions" {
   cluster_name  = aws_eks_cluster.main.name
-  principal_arn = data.aws_caller_identity.current.arn
-  type          = "STANDARD"   
+  principal_arn = "arn:aws:iam::569033310103:user/moaz-cicd"
+  type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "github_actions_admin" {
   cluster_name  = aws_eks_cluster.main.name
-  principal_arn = data.aws_caller_identity.current.arn
-  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::569033310103:user/moaz-cicd"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   access_scope {
     type = "cluster"
   }
