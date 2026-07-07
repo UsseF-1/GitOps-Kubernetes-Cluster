a43685cc5edba804fe83533e14bfcf12344b4524
shahdelwan0 <shahdelwan2024@gmail.com>
2026-07-08 00:08:24 +0300

    🌹 Add EKS access entries and policy associations for infrastructure engineer

--- terraform/eks-access.tf
+++ terraform/eks-access.tf
@@ -13,6 +13,20 @@ resource "aws_eks_access_policy_association" "github_actions_admin" {
   }
 }
 
+resource "aws_eks_access_entry" "infra_engineer" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::569033310103:user/infrastructure-engineer"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "infra_engineer_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_eks_access_entry.infra_engineer.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  access_scope {
+    type = "cluster"
+  }
+}
 resource "aws_eks_access_entry" "yosef_team_leader" {
   cluster_name  = aws_eks_cluster.main.name
   principal_arn = "arn:aws:iam::569033310103:user/yosef-team-leader"
