e819019e96f5da7773a44a30a4d5c14664d468be
shahdelwan0 <shahdelwan2024@gmail.com>
2026-07-07 06:52:11 +0300

    🌹🌹🌹🌹🌹🌹🌹 refactor: update .gitignore and enhance EBS CSI driver IAM policies

--- terraform/ebs-csi.tf
+++ terraform/ebs-csi.tf
@@ -1,15 +1,35 @@
+
+data "aws_eks_cluster" "main" {
+  name = aws_eks_cluster.main.name
+}
+
+data "tls_certificate" "eks" {
+  url = data.aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = data.aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
 data "aws_iam_policy_document" "ebs_csi_trust_policy" {
   statement {
     effect = "Allow"
     principals {
-      type        = "Service"
-      identifiers = ["pods.eks.amazonaws.com"]
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
     }
-    actions = ["sts:AssumeRole"]
+    actions = ["sts:AssumeRoleWithWebIdentity"]
     condition {
       test     = "StringEquals"
-      variable = "eks:pod-identity"
-      values   = ["true"]
+      variable = "${replace(data.aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
     }
   }
 }
@@ -19,20 +39,48 @@ resource "aws_iam_role" "ebs_csi_driver" {
   assume_role_policy = data.aws_iam_policy_document.ebs_csi_trust_policy.json
 }
 
+resource "aws_iam_policy" "ebs_csi_driver" {
+  name        = "${var.project_name}-ebs-csi-driver-policy"
+  description = "Policy for EBS CSI driver"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:AttachVolume",
+          "ec2:CreateSnapshot",
+          "ec2:CreateTags",
+          "ec2:CreateVolume",
+          "ec2:DeleteSnapshot",
+          "ec2:DeleteVolume",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeTags",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeVolumesModifications",
+          "ec2:DetachVolume",
+          "ec2:ModifyVolume",
+          "ec2:ModifyVolumeAttribute",
+          "ec2:DescribeInstanceTypes"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy"
+  policy_arn = aws_iam_policy.ebs_csi_driver.arn
   role       = aws_iam_role.ebs_csi_driver.name
 }
 
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name                = aws_eks_cluster.main.name
-  addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.40.0-eksbuild.1"
-  service_account_role_arn    = aws_iam_role.ebs_csi_driver.arn
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  tags = {
-    Name = "${var.project_name}-ebs-csi-driver"
-  }
+  cluster_name             = aws_eks_cluster.main.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.40.0-eksbuild.1"
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+  resolve_conflicts_on_create = "NONE"   
+  resolve_conflicts_on_update = "NONE"   
 }
