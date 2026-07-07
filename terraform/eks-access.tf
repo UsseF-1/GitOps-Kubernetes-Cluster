26de07f39325bcf176f6af24e72f0c1af837aca2
UsseF-1 <youseefahmed425@gmail.com>
2026-07-07 17:50:44 +0300

    Youssef Add Access for EKS

--- terraform/eks-access.tf
+++ terraform/eks-access.tf
@@ -12,3 +12,19 @@ resource "aws_eks_access_policy_association" "github_actions_admin" {
     type = "cluster"
   }
 }
+
+resource "aws_eks_access_entry" "yosef_team_leader" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::569033310103:user/yosef-team-leader"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "yosef_team_leader_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::569033310103:user/yosef-team-leader"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
