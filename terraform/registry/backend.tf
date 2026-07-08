2f1dac3f5a71da1db24ad2fa7e2aad2cce09d8b4
UsseF-1 <youseefahmed425@gmail.com>
2026-07-08 15:24:47 +0300

    Youssef Fix space problem in S3 name

--- terraform/registry/backend.tf
+++ terraform/registry/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "voting-app-s3-ecr "
+        bucket = "voting-app-s3-ecr"
         key    = "dev/terraform.tfstate"
         region = "us-east-1"
         encrypt        = true
